aed533cbc1bd9a327def9da58c46235be9e6fcf1
MarcosFerrao <marcos.sferrao.com>
2021-04-29 00:41:05 -0300

    Project 03 - CPy3 - Finalizado todas as funções do jogo HangMan(Jogo da Forca).

--- helpers.py
+++ helpers.py
@@ -20,12 +20,12 @@ def verifica_letra_informada(palavra_secreta, suas_tentativas, tentativa):
     :param tentativas: letras inseridas nesta jogada
     :return: retorna um status
     """
-    status = '' # o estatus precisa ser zerad toda vz que for chamado.
-    acertos = 0 # tambem precisa ser zerado para cada tentativa.
+    status = ''  # o estatus precisa ser zerad toda vz que for chamado.
+    acertos = 0  # tambem precisa ser zerado para cada tentativa.
 
     for letra in palavra_secreta:
         if letra.lower() in suas_tentativas:
-            status += letra # random word (Peru) letra informada 'e' mostra *e**
+            status += letra  # random word (Peru) letra informada 'e' mostra *e**
         else:
             status += '*'
         if letra.lower() == tentativa.lower():
@@ -33,6 +33,7 @@ def verifica_letra_informada(palavra_secreta, suas_tentativas, tentativa):
     print(f"\n - Acertou {acertos} letra(s) '{tentativa}'")
 
     return status
+
 
 def total_tentativas(palavra_secreta):
     """
@@ -42,3 +43,45 @@ def total_tentativas(palavra_secreta):
     """
     chances = len(palavra_secreta)
     return chances + TENTATIVAS_ADICIONAIS
+
+
+def jogo(palavra_secreta):
+    """
+    Função principal do jogo
+    :param palavra_secreta: palavra secreta gerada a partir do arquivo texto
+    :return:
+    """
+    chute = 0
+    adivinhado = False
+    suas_tentativas = []
+    chances = total_tentativas(palavra_secreta)
+    total_chances = chances
+
+    print(f"Total de chances: {chances}")
+    while chute < total_chances:
+        letra_tentativa = input("\nInforme uma letra: ")
+
+        # diminui as chances 1 a cada letra informada.
+        chances -= 1
+
+        # se a letra já foi informada ou adivnhada
+        if letra_tentativa in suas_tentativas:
+            print(f" *** ATENÇÃO ***\n Você já tentou essa letra.")
+            pass
+        elif len(letra_tentativa) == 1:  # precisa ser 1 letra somente na tentativa
+            suas_tentativas.append(letra_tentativa)  # Adicionando as letras no local correto da palavra
+            resultado = verifica_letra_informada(palavra_secreta, suas_tentativas, letra_tentativa)
+            if resultado == palavra_secreta:
+                adivinhado = True
+                print(f"\n=== Parabéns, você venceu! A palavra é: '{palavra_secreta}'. ===")
+                break
+            else:
+                print(f" - {' '.join(resultado)}")
+        else:
+            print(f"Entrada incorreta, informe somente '1' letra")
+
+        print(f" - Tentativas restantes: {chances}")  # Mostra quantas tentativas restam
+        chute += 1
+
+    if chute == total_chances:
+        print(f"*** Suas chances acabaram. A palavra secreta é : '{palavra_secreta}' ***")
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from helpers import gerar_palavra_secreta
+from helpers import gerar_palavra_secreta, jogo
 
 if __name__ == '__main__':
     palavra_secreta = gerar_palavra_secreta()
-    print(palavra_secreta)
-
+   #print(palavra_secreta) sem esse comentario é possivel ver  palavra secreta.
     # for loop que imprim palavras escondicda com *****
     print("\n==== JOGO DA FORCA ====")
     print("\nA palavra é: \n")
